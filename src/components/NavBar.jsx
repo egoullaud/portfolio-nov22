b380c24a800b215c5ac954e04d782a2ef00b2dce
elizabeth.goullaud <egoullaud@gmail.com>
2023-02-03 13:15:27 -0800

    updated resume

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
-import resume from '../assets/Resume-nov2022.pdf'
+import resume from '../assets/lee-goullaud-resume.pdf'
 
 function NavBar() {
     const [nav, setNav] = useState(false);
