086bd5084dbe09ca40ac3af0728c6d858fba8377
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-06 22:16:54 -0700

    formatted mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ function NavBar() {
   };
 
   return (
-    <div className="fixed top-0 left-0 right-0">
+    <div className="fixed top-0 left-0 right-0 z-50">
       <div
         className="
                 flex justify-between items-center
@@ -68,24 +68,39 @@ function NavBar() {
               : "z-10 fixed left-0 top-0 bg-white h-[100%] w-[60%] text-black border-r-[1px] border-r-black ease-in-out duration-500"
           }
         >
-          <h1 className="pl-4 mb-4 h-[10vh] bg-black text-white flex items-center">
-            <a href="#home">Lee Goullaud</a>
+          <h1 className="flex items-center pl-4 text-2xl italic font-Lora bg-black text-white h-[8vh]">
+            <a href="#home">friendlyDevBro</a>
+
+            <a
+              className="mx-2 pt-1 text-xl"
+              href="https://www.linkedin.com/in/lee-goullaud/"
+              target="_blank"
+            >
+              <FaLinkedinIn />
+            </a>
+            <a
+              className="mx-1 pt-1 text-xl"
+              href="https://github.com/egoullaud"
+              target="_blank"
+            >
+              <FaGithub />
+            </a>
           </h1>
           <ul className="flex flex-col justify-start w-[100%] text-black">
-            <li className="p-4 mx-2  border-b-2 border-gray-200 hover:scale-105 hover:duration-300 hover:ease-in-out ">
+            <li className="p-4 px-2  border-b-2 border-gray-200 hover:bg-black hover:bg-opacity-80 hover:text-white hover:duration-300 hover:ease-in-out ">
               <a href="#home">Home</a>
             </li>
-            <li className="p-4 mx-2  border-b-2 border-gray-200 hover:scale-105 hover:duration-300 hover:ease-in-out ">
+            <li className="p-4 px-2  border-b-2 border-gray-200 hover:bg-black hover:bg-opacity-80 hover:text-white hover:duration-300 hover:ease-in-out  ">
               <a href="#projects">Projects</a>
             </li>
-            <li className="p-4 mx-2  border-b-2 border-gray-200 hover:scale-105 hover:duration-300 hover:ease-in-out ">
+            <li className="p-4 px-2  border-b-2 border-gray-200 hover:bg-black hover:bg-opacity-80 hover:text-white hover:duration-300 hover:ease-in-out  ">
               <a href="#about">About</a>
             </li>
-            <li className="p-4 mx-2  border-b-2 border-gray-200 hover:scale-105 hover:duration-300 hover:ease-in-out ">
+            <li className="p-4 px-2  border-b-2 border-gray-200 hover:bg-black hover:bg-opacity-80 hover:text-white hover:duration-300 hover:ease-in-out  ">
               <a href="#contact">Contact</a>
             </li>
-            <li className="p-4 mx-2  hover:scale-105 hover:duration-300 hover:ease-in-out ">
-              <button className="bg-[#69a1ac] py-1 px-4 rounded-md shadow-md hover:bg-[#89c5cc]  hover:scale-100 hover:duration-300 hover:ease-in-out">
+            <li className="p-4 px-2  hover:scale-105 hover:duration-300 hover:ease-in-out ">
+              <button className="bg-[#69a1ac] py-1 px-4 rounded-md shadow-md hover:bg-[#89c5cc]  hover:duration-300 hover:ease-in-out">
                 {" "}
                 <a href={resume} target="_blank">
                   Resume
